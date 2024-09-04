82b3600ed0c666da46622cd988214e851e2318fe
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-09-05 01:33:26 +0300

    First Windows Forms Application

--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -16,5 +16,33 @@ namespace WindowsFormsApp
         {
             InitializeComponent();
         }
+        private void ChangeValue()
+        {
+            textBox2.Text = textBox1.Text;
+            if (textBox1.Text != "")
+            {
+                textBox1.ForeColor = Color.White;
+                textBox1.BackColor = Color.GreenYellow;
+                textBox2.ForeColor = Color.White;
+                textBox2.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                textBox1.ForeColor = Color.White;
+                textBox1.BackColor = Color.Red;
+                textBox2.ForeColor = Color.White;
+                textBox2.BackColor = Color.Red;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeValue();
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            ChangeValue();
+
+        }
     }
 }
